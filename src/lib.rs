e87c1f9bb43d14f88a18c339cc2fecf3912c807c
Yann Jorelle <yannjorelle@protonmail.com>
2021-03-28 22:05:19 +0300

    Add arch, shell and memory info
    Adds information about the CPU architechture, default shell and memory
    usage.

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,82 @@
+use ::std::env;
 use std::collections::HashMap;
 use std::fs::read_to_string;
 
 const OS_RELEASE_PATH: &str = "/etc/os-release";
+const MEM_USAGE_PATH: &str = "/proc/meminfo";
 
-/// If successful returns a HashMap containing key value pairs found in
-/// /etc/os-release
-fn parse_os_release_file<'a>() -> Result<HashMap<String, String>, &'a str> {
+/// Parses a file containing key - value pairs into a HashMap.
+fn parse_to_hashmap(filepath: &str, delimiter: char) -> Result<HashMap<String, String>, String> {
     let mut release_dict = HashMap::new();
-    if let Ok(contents) = read_to_string(OS_RELEASE_PATH) {
+    if let Ok(contents) = read_to_string(filepath) {
         for line in contents.lines() {
-            let split = line.split('=').collect::<Vec<_>>();
+            let split = line.split(delimiter).collect::<Vec<_>>();
             if split.len() < 2 {
-                return Err("Failed to parse line in os-release file.");
+                return Err(format!("Failed to parse line in {}.", filepath));
             }
-            release_dict.insert(split[0].to_owned(), split[1].trim_matches('"').to_owned());
+            release_dict.insert(split[0].to_owned(), split[1].to_owned());
         }
         Ok(release_dict)
     } else {
-        Err("Failed to read os-release file.")
+        Err(format!("Failed to read {}.", filepath))
     }
 }
 
-pub fn get_distro() -> String {
-    let default_name = String::from("Unknown");
-    if let Ok(release_dict) = parse_os_release_file() {
-        let distro_name = release_dict.get("NAME").unwrap_or(&default_name);
-        if let Some(version) = release_dict.get("VERSION") {
-            return format!("{} {}", distro_name, version);
+/// Retrieves the distro name from /etc/os-release
+pub fn get_os() -> Option<String> {
+    let release_map = match parse_to_hashmap(OS_RELEASE_PATH, '=') {
+        Ok(map) => map,
+        Err(e) => {
+            eprintln!("{}", e);
+            return None;
         }
-        distro_name.to_string()
-    } else {
-        default_name
+    };
+    let distro_name = release_map.get("NAME")?.trim_matches('"');
+    if let Some(version) = release_map.get("VERSION") {
+        return Some(format!("OS: {} {}", distro_name, version));
     }
+    Some(format!("OS: {}", distro_name))
+}
+
+pub fn get_cpu_architechture() -> Option<String> {
+    Some(format!("Arch: {}", env::consts::ARCH))
+}
+
+pub fn get_shell() -> Option<String> {
+    match env::var("SHELL") {
+        Ok(shell) => Some(format!("Shell: {}", shell)),
+        Err(e) => {
+            eprintln!("Failed to detect shell, {}", e);
+            None
+        }
+    }
+}
+
+/// Parses a string value from /proc/meminfo into a u32 containing the size
+/// in MiB.
+fn parse_mem_value(value: &str) -> u32 {
+    let kb_value = value
+        .trim_matches(|c: char| c == 'k' || c == 'B' || c.is_whitespace())
+        .parse::<u32>()
+        .unwrap();
+    let mb_value = kb_value / 1024;
+    mb_value
+}
+
+pub fn get_memory_usage() -> Option<String> {
+    let mem_map = match parse_to_hashmap(MEM_USAGE_PATH, ':') {
+        Ok(map) => map,
+        Err(e) => {
+            eprintln!("{}", e);
+            return None;
+        }
+    };
+    let mem_total = parse_mem_value(mem_map.get("MemTotal")?);
+    let mem_available = parse_mem_value(mem_map.get("MemAvailable")?);
+
+    Some(format!(
+        "Memory: {}MiB / {}MiB",
+        mem_total - mem_available,
+        mem_total
+    ))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use minifetch::{get_distro};
+use minifetch::{get_cpu_architechture, get_memory_usage, get_os, get_shell};
 
 fn main() {
-    let distro = get_distro();
-    println!("OS: {}", distro);
+    let os = get_os();
+    let arch = get_cpu_architechture();
+    let shell = get_shell();
+    let memory = get_memory_usage();
+    
+    for val in [os, arch, shell, memory].iter() {
+        if let Some(value) = val {
+            println!("{}", value);
+        }
+    }
 }
